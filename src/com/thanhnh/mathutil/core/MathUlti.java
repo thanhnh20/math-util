ba64dc29f600e55c661a6628be17d99da20cd844
thanhnh20 <huuthanh10010308@gmail.com>
2022-06-15 11:54:55 +0700

    refactor the main code and green 4ever always

--- src/com/thanhnh/mathutil/core/MathUlti.java
+++ src/com/thanhnh/mathutil/core/MathUlti.java
@@ -23,7 +23,8 @@ public class MathUlti {
     // 20 giai thừa là 18 con số 0, vừa kịp cho kiểu long của java
     // 21! tràn của long
     // quy ước tính n! trong khoảng từ 0-20
-    public static long getFactorial(int n){
+    
+    /*public static long getFactorial(int n){
         if(n<0 || n>20){
             throw new IllegalArgumentException("Invalid argumment. N must be between 0..20");
         }
@@ -36,6 +37,32 @@ public class MathUlti {
             product *= i;
         }
         return product;
+    }*/
+    
+    public static long getFactorial(int n){
+        if(n<0 || n>20){
+            throw new IllegalArgumentException("Invalid argumment. N must be between 0..20");
+        }
+        if(n == 0 || n == 1){
+            return 1; // kết thúc cuộc chơi sớm nếu nhận được đầu vào đặc biệt         
+        }
+  
+        return n * getFactorial(n-1); // công thức đệ quy
     }
     
+    
+    // Học đệ quy trong vòng 30s - RECURSION
+    // Hiện tượng gọi lại chính mình với 1 quy mô khác
+    // VD: con búp bê Nga, giống nhau và lồng trong nau
+    // búp bê to, nhỏ hơn, nhỏ hơn nữa ,.... đến điểm dừng
+    
+    // tính giùm tui 6!
+    // n! = 1.2.3.4.5.6. ... .n
+    // 6! = 6 * 5!
+    // 5! = 5 * 4!
+    // ....
+    // 2! = 2 * 1! // điểm dừng
+    // quy ước 1! = 0! = 1;
+    
+    //chốt hạ: n! = n * (n-1)!
 }
